e97b06b56b21f9bdbd28413152428e80b4bc947d
Aljaz <aljazsustar99@gmail.com>
2021-04-10 11:55:20 +0200

    Added packages to install, change shell to zsh

---
+++ .screenlayout/layout1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ xrandr -s 1920x1080 --primary#!/bin/sh
+ xrandr -s 1920x1080 --primary#!/bin/sh
+ xrandr -s 1920x1080 --primary#!/bin/sh
+ xrandr -s 1920x1080 --primary
--- install.sh
+++ install.sh
@@ -21,28 +21,29 @@ read isLaptop
 cd .screenlayout || mkdir .screenlayout && cd .screenlayout
 if [[ $isLaptop == "y" ]] || [[ -z $isLaptop ]] 
 then
-    printf "#!/bin/sh\n xrandr -s 1920x1080 --primary" >> layout1.sh
+    printf "#!/bin/sh\n xrandr -s 1920x1080" >> layout1.sh
 else
-    printf "#!/bin/sh\n xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate right --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-4 --off --output DP-5 --off" >> layout.sh
+    printf "#!/bin/sh\n xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate right --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-4 --off --output DP-5 --off" >> layout1.sh
 fi
 
-sudo chmod +x layout.sh
+sudo chmod +x layout1.sh
+./layout1.sh
 cd || exit 1
 
 echo "Installing extra software"
 
-yay -S intellij-idea-ultimate-edition pycharm-ultimate clion webstorm
-sudo pacman -S jdk-openjdk python-pip spotify alacritty nitrogen visual-studio-code-bin
+yay -S intellij-idea-ultimate-edition pycharm-ultimate clion webstorm oh-my-zsh-git
+sudo pacman -S jdk-openjdk python-pip spotify alacritty nitrogen visual-studio-code-bin zsh brave-bin
 
 cd awesome-gruvbox || exit 1
 
 #add awesome-wm-widgets, spicetify-cli, spicetify-themes, rofi-themes, 
 echo "Copying config files"
-cp alacritty ~/.config/. && cp awesome ~/.config/. && copy .Xresources ../. && copy .zshrc ../. || echo "Failed to copy config files" && exit 1
+cp -r alacritty ~/.config/. && cp -r awesome ~/.config/. && cp .Xresources ../. && cp .zshrc ../. || echo "Failed to copy config files"
 cd || exit 1
 echo "Installing awesome-wm widgets"
-cd ~/.config/awesome && git clone https://github.com/streetturtle/awesome-wm-widgets.git || echo "Failed to install awesome-wm-widgtes" && exit 1
-cd || exit 1
+cd ~/.config/awesome && git clone https://github.com/streetturtle/awesome-wm-widgets.git || echo "Failed to install awesome-wm-widgtes" 
+cd ~
 
 echo "Installing spicetify-cli"
 yay -S spicetify-cli 
@@ -68,7 +69,7 @@ spicetify apply
 cd || exit 1
 
 echo "Installing rofi themes"
-git clone --depth=1 https://github.com/adi1090x/rofi.git || echo "Failed to clone the repo" && exit 1
+git clone --depth=1 https://github.com/adi1090x/rofi.git || echo "Failed to clone the repo"
 cd rofi || exit 1
 sudo chmod +x setup .sh
 ./setup.sh
@@ -77,3 +78,6 @@ cd || exit 1
 echo "Downloading wallpapers"
 cd Pictures || mkdir Pictures && cd Pictures
 git clone "https://gitlab.com/dwt1/wallpapers.git"
+
+echo "Changing default shell to zsh"
+chsh -s /bin/zsh
