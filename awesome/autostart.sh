170ce00e42a75a2e2cc2212c6326181cfcf9f7fe
Aljaz <aljazsustar99@gmail.com>
2021-05-13 15:44:30 +0200

    updated autostart

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -10,7 +10,7 @@ run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 #run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 run nm-applet
-run picom -b --config  "$HOME"/.config/awesome/picom.conf
+run picom -b --config  home/aljaz/.config/awesome/picom.conf
 run "$HOME"/.screenlayout/layout1.sh
 run flameshot
 #run caffeine
@@ -22,10 +22,10 @@ run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 #run numlockx on
 #run volumeicon
 #run nitrogen --restore
-run conky -c $HOME/.config/awesome/system-overview
+run conky -c HOME/.config/awesome/system-overview
 #you can set wallpapers in themes as well
 #feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-feh --bg-scale --randomize home/aljaz/Pictures/wallpapers
+feh --bg-scale --randomize /home/aljaz/Pictures/wallpapers
 #run applications from startup
 #run firefox
 #run atom
